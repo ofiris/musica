6fa6216cdad29e54a7d9a7dd7006f03623bbf090
royeet <royee.tichauer@huji.ac.il>
2013-07-03 11:50:48 +0300

    adding dirict wifi connection

--- src/postpc/musica/WelcomeActivity.java
+++ src/postpc/musica/WelcomeActivity.java
@@ -1,15 +1,39 @@
 package postpc.musica;
 
+import java.util.Collection;
+
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
 import android.view.Menu;
 
-public class WelcomeActivity extends Activity {
 
+
+public class WelcomeActivity extends Activity {
+	WifiP2pManager mManager;
+	Channel mChannel;
+	BroadcastReceiver mReceiver;
+	IntentFilter mIntentFilter;
+	Collection<WifiP2pDevice> currentDevices;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+		mIntentFilter = new IntentFilter();
+		mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+		mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+		mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+		mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+		mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+		mChannel = mManager.initialize(this, getMainLooper(), null);
+		mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+		
 	}
 
 	@Override
