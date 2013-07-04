d7f1b5c803728b375ba1f76ba84d0d1fb63c3860
lior bar <lior656@gmail.com>
2013-07-04 10:52:52 +0300

    merging 1

--- gen/postpc/musica/R.java
+++ gen/postpc/musica/R.java
@@ -22,6 +22,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int roundedbutton=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080009;
@@ -51,15 +52,20 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int back=0x7f05000b;
-        public static final int empty=0x7f05000d;
+        public static final int empty=0x7f05000f;
         public static final int hello_world=0x7f050002;
         public static final int hint=0x7f050007;
         public static final int initText=0x7f050009;
         public static final int next=0x7f05000a;
+        public static final int play=0x7f05000e;
         public static final int prev=0x7f05000c;
         public static final int search=0x7f050008;
+        public static final int sender=0x7f050010;
+        public static final int title_activity_master=0x7f05000d;
         public static final int title_activity_search=0x7f050006;
+        public static final int title_activity_slave_play=0x7f050011;
         public static final int title_activity_waiting=0x7f050005;
+        public static final int waitForStart=0x7f050012;
         public static final int wait_for_song=0x7f050004;
     }
     public static final class style {
--- src/postpc/musica/Consts.java
+++ src/postpc/musica/Consts.java
@@ -3,4 +3,8 @@ package postpc.musica;
 public class Consts {
 	public static int searchNum = 5;
 	
+	public static void jumpToPlay(){
+		
+	}
+	
 }
